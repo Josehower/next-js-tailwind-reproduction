1cbe815bb364fa743511ae7f4dbc0d3b8413f2e4
Josehower <josehowerupn@gmail.com>
2023-04-05 14:38:23 +0200

    4 files modified

--- server/build/server/index.js
+++ server/build/server/index.js
@@ -3,9 +3,9 @@ import express from 'express';
 import { nextRequestHandler } from '../next-app/util/server.js';
 const app = express();
 const port = 3000;
-app.get('/', async (req, res) => {
-    await nextRequestHandler(req, res);
+app.use('/', async (req, res) => {
+  await nextRequestHandler(req, res);
 });
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
